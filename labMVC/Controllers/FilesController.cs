a40e8e6d691142b63a1a6efb460ed7eec60ac8a3
Liang Zhao <zhaoliangbz@gmail.com>
2017-09-18 21:23:47 -0700

    lab2 updated

---
+++ labMVC/Controllers/FilesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace labMVC.Controllers
+{
+    public class FilesController : Controller
+    {
+        // GET: Files
+        public ActionResult Index()
+        {
+			string[] files = Directory.GetFiles(Server.MapPath("~/TextFiles"));
+			ViewBag.files = files;
+			return View();
+        }
+		// GET: File contents
+		public ActionResult Contents(string id)
+		{
+			var path = Server.MapPath($"~/TextFiles/{id}.txt");
+			ViewBag.details = System.IO.File.ReadAllText(path);
+			return View();
+		}
+	}
+}
